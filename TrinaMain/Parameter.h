4e9a7dfeaac408b65e0642792b03598bc6e5237b
augustin_zahorka <augustin.zahorka@gmail.com>
2025-05-19 18:45:58 +0200

    Test commit terminal

--- TrinaMain/Parameter.h
+++ TrinaMain/Parameter.h
@@ -2,11 +2,11 @@
 #define PARAMETER_H
 
 // Déclaration des variables globales
-extern char ControlerStatus;
-extern int x; // lateral translation
+extern char ControlerStatus; // Variable de renvoi qui me dit dans quel mode je suis.
+extern int x; // lateral translation mm
 extern int y; // depth translation
-extern int rh; // horizontal rotation
-extern int rv; // vertical translation
+extern int rh; // horizontal rotation °*10^-2
+extern int rv; // vertical rotation
 extern bool ManualMove; //hand manual move from the Camera
 extern bool AutoMode;
 
--- TrinaMain/TrinaMain.ino
+++ TrinaMain/TrinaMain.ino
@@ -13,12 +13,12 @@ void loop()
   Serial.begin(9600);
 
   // define the parameter
-  x = 3;
+  x = 3; // mm
   y = 4;
-  rh = 5;
+  rh = 5; // °*10^-2
   rv = 6;
-  AutoMode = false;
-  ManualMove = false;
+  AutoMode = false; // Quand t'es en mode auto il se déplace
+  ManualMove = false; // Quand il y a un déplacement en cours (c'est quand c'est appuyé sur les boutons)
 
   UpdateControlerStatus();
   delay(2000);
